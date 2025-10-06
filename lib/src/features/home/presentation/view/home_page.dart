1a1443b804ba46c68205ec72aeb20c395185e9a1
HenryBlairG <henry.blair.g@gmail.com>
2025-10-05 22:41:10 -0300

    ADD about and skills base views

--- lib/src/features/home/presentation/view/home_page.dart
+++ lib/src/features/home/presentation/view/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shadcn_ui/shadcn_ui.dart';
+// import 'package:shadcn_ui/shadcn_ui.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({
---
+++ lib/src/features/skills/presentation/view/skills_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SkillsPage extends StatelessWidget {
+  const SkillsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Skills Page'),
+    );
+  }
+}
